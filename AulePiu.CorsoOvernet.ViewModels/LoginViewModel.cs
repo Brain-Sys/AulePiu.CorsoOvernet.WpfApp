da105ad08266eb0d21eb5a70afa3e62ab3d459e6
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-06-26 08:36:19 +0200

    Add project files.

---
+++ AulePiu.CorsoOvernet.ViewModels/LoginViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace AulePiu.CorsoOvernet.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private Timer timer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // PascalCase
+        // public string Username { get; set; }
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password { get; set; }
+        public bool SaveCredentials { get; set; }
+
+        // Fields (NO), mettere get & set per fare una property
+        public DateTime CurrentTime { get; set; }
+
+        public LoginViewModel()
+        {
+            this.Username = "igor";
+            this.Password = "pwd";
+            this.CurrentTime = DateTime.Now;
+
+            timer = new Timer(updateTimer, null, 0, 1000);
+        }
+
+        private void updateTimer(object state)
+        {
+            this.CurrentTime = DateTime.Now;
+
+            // C'è qualcuno sottoscritto a questo evento?
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this,
+                    new PropertyChangedEventArgs(nameof(CurrentTime)));
+            }
+        }
+
+        public void CheckAuth()
+        {
+
+        }
+    }
+}
---
+++ AulePiu.CorsoOvernet.WpfApp/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AulePiu.CorsoOvernet.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var ex = e.Exception;
+            e.Handled = true;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string[] arguments = e.Args;
+            base.OnStartup(e);
+
+            var wnd = new LoginWindow();
+            wnd.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            e.ApplicationExitCode = 0;
+        }
+    }
+}
